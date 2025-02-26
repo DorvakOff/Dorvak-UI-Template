52d29c851998b7953804aeb2e775f9d51fc57ad4
Matéo Papaïx (Dorvak) <mateo.papaix@gmail.com>
2025-02-26 18:37:53 +0100

    Init repo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {DorvakUiModule} from 'dorvak-ui';
+import {LucideAngularModule} from 'lucide-angular';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    DorvakUiModule,
+    LucideAngularModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Dorvak-UI-Template';
+
+  openUrl(url: string) {
+    window.open(url, '_blank');
+  }
 }
