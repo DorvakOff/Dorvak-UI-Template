67205626a6f5abd90cd6e1c064343c368edeb37d
Matéo Papaïx (Dorvak) <mateo.papaix@gmail.com>
2025-02-27 23:45:31 +0100

    Migration v4

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,11 @@
-import {ApplicationConfig, importProvidersFrom, provideZoneChangeDetection} from '@angular/core';
+import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import {icons, LucideAngularModule} from 'lucide-angular';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    importProvidersFrom(LucideAngularModule.pick(icons))
   ]
 };
